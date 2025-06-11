4a754f7719b399f404919ba0243d7db4bb6946d9
markilius508 <prawit.jamparueang@gmx.de>
2025-06-11 09:31:30 +0200

    Add behavior_tree action node nav2_navigate_through_gps_poses_action

--- nav2_bt_navigator/src/bt_navigator.cpp
+++ nav2_bt_navigator/src/bt_navigator.cpp
@@ -69,7 +69,7 @@ BtNavigator::BtNavigator(rclcpp::NodeOptions options)
     "nav2_goal_updated_controller_bt_node",
     "nav2_is_battery_low_condition_bt_node",
     "nav2_navigate_through_poses_action_bt_node",
-    // "nav2_navigate_through_gps_poses_action_bt_node",
+    "nav2_navigate_through_gps_poses_action_bt_node",
     "nav2_navigate_to_pose_action_bt_node",
     "nav2_remove_passed_goals_action_bt_node",
     "nav2_planner_selector_bt_node",
